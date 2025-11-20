1804dc3fbec94c76b14c6a0baed311f13462f894
Clase <200045@iessanalberto.com>
2025-11-20 13:19:48 +0100

    Commit1

---
+++ Juego.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import random
+from tkinter import messagebox
+
+from PersonajesPython.Clases.Personaje import Personaje
+
+
+
+
+
+PersonajePrueba1 = Personaje("Canvas", "Prim", 10,10)
+PersonajePrueba1 = Personaje("Canvas", "Espartero", 11,10)
+PersonajePrueba1.probar()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PersonajesPython
+
 Programa para practicar POO en I2
+Buenas. En este proyecto python vamos a parcticar eso de los personajes que aun no sabemos muy bien como va. Esperemos que aprendamos mucho.
+
+# Espero que os guete. Un beso. 
+
+# 
